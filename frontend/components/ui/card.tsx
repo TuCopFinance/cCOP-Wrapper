103a8f806e7923a25ba3b99a60baaa08279ace25
jistro <contact@jistro.xyz>
2025-07-09 18:20:54 -0600

    chore: remove frontend files

--- frontend/components/ui/card.tsx
+++
@@ -1,8 +0,0 @@
-export function Card({ title, children }) {
-  return (
-    <div className="bg-white p-6 rounded-2xl shadow-md space-y-3 border">
-      <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
-      <div>{children}</div>
-    </div>
-  );
-}
