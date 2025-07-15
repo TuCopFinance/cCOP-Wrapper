abe5c161cbdde4eb6b6294b602d15c5deb0a3dcb
jistro <contact@jistro.xyz>
2025-07-14 19:12:22 -0600

    refactor: improve code formatting and organization in WrappedCCOPScript contract

--- contracts/script/WrappedCCOP.s.sol
+++ contracts/script/WrappedCCOP.s.sol
@@ -6,11 +6,19 @@ import {WrappedCCOP} from "../src/WrappedCCOP.sol";
 
 contract WrappedCCOPScript is Script {
     WrappedCCOP public wrappedCCOP;
-    address public treasuryAddress = 0xAF4387cC9105C9B716B9B84F673996dCa7ac5150; // Example treasury address
-    
+
     address public constant OWNER = 0x5cBf2D4Bbf834912Ad0bD59980355b57695e8309; // Mailbox for Base Sepolia (testnet)
-    address public constant MAILBOX_BASE_SEPOLIA = 0x6966b0E55883d49BFB24539356a2f8A673E02039; // Mailbox for Base Sepolia (testnet)
-    address public constant MAILBOX_BASE_MAINNET = 0xeA87ae93Fa0019a82A727bfd3eBd1cFCa8f64f1D; // Mailbox for Base Mainnet
+    
+    address public constant MAILBOX_BASE_SEPOLIA =
+        0x6966b0E55883d49BFB24539356a2f8A673E02039; // Mailbox for Base Sepolia (testnet)
+    address public constant MAILBOX_BASE_MAINNET =
+        0xeA87ae93Fa0019a82A727bfd3eBd1cFCa8f64f1D; // Mailbox for Base Mainnet
+
+    uint32 public constant DOMAIN_ID_BASE_SEPOLIA = 84532; // Domain ID for Base Sepolia
+    uint32 public constant DOMAIN_ID_BASE_MAINNET = 8453; // Domain ID for Base Mainnet
+
+    address public constant TREASURY_ADDRESS =
+        0xAF4387cC9105C9B716B9B84F673996dCa7ac5150; // Example treasury address
 
     function setUp() public {}
 
@@ -20,14 +28,20 @@ contract WrappedCCOPScript is Script {
         wrappedCCOP = new WrappedCCOP(
             OWNER,
             block.chainid == 84532
-                ? MAILBOX_BASE_SEPOLIA // mailbox for Base Sepolia (testnet)
-                : MAILBOX_BASE_MAINNET // mailbox for Base Mainnet
+                ? MAILBOX_BASE_SEPOLIA
+                : MAILBOX_BASE_MAINNET,
+            block.chainid == 84532
+                ? DOMAIN_ID_BASE_SEPOLIA
+                : DOMAIN_ID_BASE_MAINNET,
+            TREASURY_ADDRESS
         );
 
+        /*
         wrappedCCOP.setTreasury(
             treasuryAddress, // treasury address
             block.chainid == 84532 ? 44787 : 42220 // domain ID for Base Sepolia or Base Mainnet
         );
+        */
 
         vm.stopBroadcast();
     }
