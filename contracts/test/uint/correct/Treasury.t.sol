da1622ac60beb8c0825b460b7494ad434dc4858e
jistro <contact@jistro.xyz>
2025-07-16 18:45:28 -0600

    feat: add new admin and wrapped token address proposal tests in Treasury contract

--- contracts/test/uint/correct/Treasury.t.sol
+++ contracts/test/uint/correct/Treasury.t.sol
@@ -11,9 +11,17 @@ import {CCOPMock} from "@cCOP_wrapper/CCOPMock.sol";
 contract TreasuryTest is Test, Constants {
     MockMailbox baseMailbox;
     MockMailbox celoMailbox;
+
+    MockMailbox mockMailbox;
+
     Treasury treasury;
     WrappedCCOP wrappedCCOP;
     CCOPMock cCOP;
+
+    CCOPMock mockCCOP;
+
+    WrappedCCOP mockWrappedCCOP;
+    WrappedCCOP newMockWrappedCCOP;
 
     function setUp() public {
         baseMailbox = new MockMailbox(domainID.baseMainnet);
@@ -90,4 +98,301 @@ contract TreasuryTest is Test, Constants {
         assertEq(admin.proposal, USER1.Address);
         assertEq(admin.timeToAccept, block.timestamp + 1 days);
     }
+
+    function test_NewAdminProposal_cancel() public mintCCOP {
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewAdminProposal(USER1.Address);
+
+        skip(20 minutes);
+
+        treasury.cancelNewAdminProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory admin = treasury
+            .getAdminStructure();
+
+        assertEq(admin.current, ADMIN.Address);
+        assertEq(admin.proposal, address(0));
+        assertEq(admin.timeToAccept, 0);
+    }
+
+    function test_NewAdminProposal_accept() public mintCCOP {
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewAdminProposal(USER1.Address);
+
+        vm.stopPrank();
+
+        skip(1 days);
+
+        vm.startPrank(USER1.Address);
+
+        treasury.acceptNewAdminProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory admin = treasury
+            .getAdminStructure();
+
+        assertEq(admin.current, USER1.Address);
+        assertEq(admin.proposal, address(0));
+        assertEq(admin.timeToAccept, 0);
+    }
+
+    function test_NewWrappedTokenAddress_directAdd() public mintCCOP {
+        mockMailbox = new MockMailbox(1);
+        mockWrappedCCOP = new WrappedCCOP(
+            ADMIN.Address,
+            address(mockMailbox),
+            domainID.celoMainnet,
+            address(treasury)
+        );
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(mockWrappedCCOP)
+        );
+
+        vm.stopPrank();
+
+        assertEq(treasury.getWrappedTokenAddress(1), address(mockWrappedCCOP));
+    }
+
+    modifier makeMockWrappedCCOP() {
+        mockMailbox = new MockMailbox(1);
+        newMockWrappedCCOP = new WrappedCCOP(
+            ADMIN.Address,
+            address(mockMailbox),
+            domainID.celoMainnet,
+            address(treasury)
+        );
+        mockWrappedCCOP = new WrappedCCOP(
+            ADMIN.Address,
+            address(mockMailbox),
+            domainID.celoMainnet,
+            address(treasury)
+        );
+        _;
+    }
+
+    function test_NewWrappedTokenAddress_propose() public makeMockWrappedCCOP {
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(mockWrappedCCOP)
+        );
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(newMockWrappedCCOP)
+        );
+
+        vm.stopPrank();
+
+        Treasury.Bytes32Proposal memory wrappedToken = treasury
+            .getWrappedTokenAddressStructure(1);
+
+        assertEq(
+            address(uint160(uint256(wrappedToken.current))),
+            address(mockWrappedCCOP)
+        );
+        assertEq(
+            address(uint160(uint256(wrappedToken.proposal))),
+            address(newMockWrappedCCOP)
+        );
+        assertEq(wrappedToken.timeToAccept, block.timestamp + 1 days);
+    }
+
+    function test_NewWrappedTokenAddress_cancel() public makeMockWrappedCCOP {
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(mockWrappedCCOP)
+        );
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(newMockWrappedCCOP)
+        );
+
+        skip(20 minutes);
+
+        treasury.cancelNewWrappedTokenAddressProposal(1);
+
+        vm.stopPrank();
+
+        Treasury.Bytes32Proposal memory wrappedToken = treasury
+            .getWrappedTokenAddressStructure(1);
+
+        assertEq(
+            address(uint160(uint256(wrappedToken.current))),
+            address(mockWrappedCCOP)
+        );
+        assertEq(address(uint160(uint256(wrappedToken.proposal))), address(0));
+        assertEq(wrappedToken.timeToAccept, 0);
+    }
+
+    function test_NewWrappedTokenAddress_accept() public makeMockWrappedCCOP {
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(mockWrappedCCOP)
+        );
+
+        treasury.proposeNewWrappedTokenAddressProposal(
+            1,
+            address(newMockWrappedCCOP)
+        );
+
+        skip(1 days);
+
+        treasury.acceptNewWrappedTokenAddressProposal(1);
+
+        vm.stopPrank();
+
+        Treasury.Bytes32Proposal memory wrappedToken = treasury
+            .getWrappedTokenAddressStructure(1);
+
+        assertEq(
+            address(uint160(uint256(wrappedToken.current))),
+            address(newMockWrappedCCOP)
+        );
+        assertEq(wrappedToken.proposal, bytes32(0));
+        assertEq(wrappedToken.timeToAccept, 0);
+    }
+
+    function test_NewMailboxAddress_propose() public {
+        mockMailbox = new MockMailbox(domainID.celoMainnet);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewMailboxAddressProposal(address(mockMailbox));
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory mailboxProposal = treasury
+            .getMailboxAddressStructure();
+
+        assertEq(mailboxProposal.current, address(celoMailbox));
+        assertEq(mailboxProposal.proposal, address(mockMailbox));
+        assertEq(mailboxProposal.timeToAccept, block.timestamp + 1 days);
+    }
+
+    function test_NewMailboxAddress_cancel() public {
+        mockMailbox = new MockMailbox(domainID.celoMainnet);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewMailboxAddressProposal(address(mockMailbox));
+
+        skip(20 minutes);
+
+        treasury.cancelNewMailboxAddressProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory mailboxProposal = treasury
+            .getMailboxAddressStructure();
+
+        assertEq(mailboxProposal.current, address(celoMailbox));
+        assertEq(mailboxProposal.proposal, address(0));
+        assertEq(mailboxProposal.timeToAccept, 0);
+    }
+
+    function test_NewMailboxAddress_accept() public {
+        mockMailbox = new MockMailbox(domainID.celoMainnet);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewMailboxAddressProposal(address(mockMailbox));
+
+        skip(1 days);
+
+        treasury.acceptNewMailboxAddressProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory mailboxProposal = treasury
+            .getMailboxAddressStructure();
+
+        assertEq(mailboxProposal.current, address(mockMailbox));
+        assertEq(mailboxProposal.proposal, address(0));
+        assertEq(mailboxProposal.timeToAccept, 0);
+    }
+
+
+    function test_NewCCOPAddress_propose() public {
+        mockCCOP = new CCOPMock(ADMIN.Address);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewCCOPAddressProposal(address(mockCCOP));
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory ccopProposal = treasury
+            .getCCOPAddressStructure();
+
+        assertEq(ccopProposal.current, address(cCOP));
+        assertEq(ccopProposal.proposal, address(mockCCOP));
+        assertEq(ccopProposal.timeToAccept, block.timestamp + 1 days);
+    }
+
+    function test_NewCCOPAddress_cancel() public {
+        mockCCOP = new CCOPMock(ADMIN.Address);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewCCOPAddressProposal(address(mockCCOP));
+
+        skip(20 minutes);
+
+        treasury.cancelNewCCOPAddressProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory ccopProposal = treasury
+            .getCCOPAddressStructure();
+
+        assertEq(ccopProposal.current, address(cCOP));
+        assertEq(ccopProposal.proposal, address(0));
+        assertEq(ccopProposal.timeToAccept, 0);
+    }
+
+    function test_NewCCOPAddress_accept() public {
+        mockCCOP = new CCOPMock(ADMIN.Address);
+
+        vm.startPrank(ADMIN.Address);
+
+        treasury.proposeNewCCOPAddressProposal(address(mockCCOP));
+
+        skip(1 days);
+
+        treasury.acceptNewCCOPAddressProposal();
+
+        vm.stopPrank();
+
+        Treasury.AddressTypeProposal memory ccopProposal = treasury
+            .getCCOPAddressStructure();
+
+        assertEq(ccopProposal.current, address(mockCCOP));
+        assertEq(ccopProposal.proposal, address(0));
+        assertEq(ccopProposal.timeToAccept, 0);
+    }
+
+    function test_toggleFuse() public {
+        vm.startPrank(ADMIN.Address);
+        treasury.toggleFuse();
+        vm.stopPrank();
+
+        assertEq(treasury.getFuse(), false);
+    }
+
 }
