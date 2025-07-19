3c2f2acea9e98613620710b1dc79b2f6ebef5c6e
jistro <contact@jistro.xyz>
2025-07-19 12:18:31 -0600

    feat: update projectId in WagmiAdapter configuration for consistency

--- dapp/src/config/index.ts
+++ dapp/src/config/index.ts
@@ -2,20 +2,18 @@ import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { celo, arbitrum, base } from "@reown/appkit/networks";
 import type { AppKitNetwork } from "@reown/appkit/networks";
 
-export const projectId = "35df4a7708f828a4182e8382ac81c981";
+export const projectId = "cc288e4b98a01891965ec3259e3d60d3";
 
 export const networks = [celo, arbitrum, base] as [
   AppKitNetwork,
   ...AppKitNetwork[]
 ];
 
-
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   ssr: true,
   projectId: projectId,
   networks: networks,
-  
 });
 
 export const config = wagmiAdapter.wagmiConfig;
