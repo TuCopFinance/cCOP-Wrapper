78a27ef2b2abe7a2e97a2a807b235b79ba5f619b
jistro <contact@jistro.xyz>
2025-07-19 00:10:01 -0600

    feat: update projectId to use environment variable for better configuration management

--- dapp/src/config/index.ts
+++ dapp/src/config/index.ts
@@ -3,7 +3,7 @@ import { celo, arbitrum, base } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 
-export const projectId = "d251071937c99e48e054a6b9b269d315"
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
@@ -14,8 +14,8 @@ export const networks = [celo, arbitrum, base] as [AppKitNetwork, ...AppKitNetwo
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   ssr: true,
-  projectId,
-  networks
+  projectId: projectId,
+  networks: networks
 })
 
 export const config = wagmiAdapter.wagmiConfig
