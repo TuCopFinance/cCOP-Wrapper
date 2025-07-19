eea7eeffcb2fb76bcb0d9933fe9c8053ea4cadea
jistro <contact@jistro.xyz>
2025-07-18 23:31:17 -0600

    feat: update projectId to a fixed value for consistent configuration

--- dapp/src/config/index.ts
+++ dapp/src/config/index.ts
@@ -2,8 +2,8 @@ import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { celo, arbitrum, base } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
-// Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+
+export const projectId = "d251071937c99e48e054a6b9b269d315"
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
