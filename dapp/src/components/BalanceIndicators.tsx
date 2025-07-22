6e864916365b2893304dde3bbe7d461b38e4e0d9
jistro <contact@jistro.xyz>
2025-07-21 20:03:36 -0600

    feat(assets): update asset images and enhance BalanceIndicators styling

--- dapp/src/components/BalanceIndicators.tsx
+++ dapp/src/components/BalanceIndicators.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { useTokenBalances } from "@/hooks/useTokenBalances";
 import { FiRefreshCcw, FiChevronDown, FiChevronUp } from "react-icons/fi";
-<<<<<<< Updated upstream
 import styles from "./BalanceIndicators.module.css";
-=======
-import styles from "./BalanceIndicatorsStyles.module.css";
-import Image from "next/image";
->>>>>>> Stashed changes
 
 export const BalanceIndicators = () => {
   const { celo, base, arb, refresh } = useTokenBalances();
@@ -47,22 +42,25 @@ export const BalanceIndicators = () => {
       {showDetails && (
         <div className={styles.listOfAssets}>
           <div className={`${styles.indicator} ${styles.celo}`}>
-            <span className={styles.label}>Celo:</span>
-            <span className={styles.value}>
+            <p>
+              Celo:{" "}
               {parseFloat(celo).toFixed(2)} cCOP
             </p>
+            <img src="assets/Celo.png" alt="cCOP Token" />
           </div>
           <div className={`${styles.indicator} ${styles.base}`}>
-            <span className={styles.label}>Base:</span>
-            <span className={styles.value}>
+            <p>
+              Base:{" "}
               {parseFloat(base).toFixed(2)} wcCOP
             </p>
+            <img src="assets/Base.png" alt="wcCOP Token" />
           </div>
           <div className={`${styles.indicator} ${styles.arb}`}>
-            <span className={styles.label}>Arbitrum:</span>
-            <span className={styles.value}>
+            <p>
+              Arbitrum:{" "}
               {parseFloat(arb).toFixed(2)} wcCOP
             </p>
+            <img src="assets/Arbitrum.png" alt="wcCOP Token"  />
           </div>
         </div>
       )}
